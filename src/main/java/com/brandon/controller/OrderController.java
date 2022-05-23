39ee8d8bbfa52d0bfc4a850afc307ddd75e940fa
brandon <dmdjqn@gmail.com>
2022-05-24 02:01:57 +0900

    feat: 데이터수집용 Contoller 추가

--- src/main/java/com/brandon/controller/OrderController.java
+++ src/main/java/com/brandon/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.brandon.controller;
 
+import com.brandon.domain.Member;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
