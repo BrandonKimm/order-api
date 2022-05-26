d8014e76c3c0b41cf6eb40132ad02c85ee939241
brandon <dmdjqn@gmail.com>
2022-05-26 15:05:13 +0900

    feat: 예외처리 기능 추가

---
+++ src/main/java/com/brandon/exception/NotEnoughStockException.java
@@ -0,0 +1,21 @@
+package com.brandon.exception;
+
+
+public class NotEnoughStockException extends RuntimeException {
+    public NotEnoughStockException(String need_more_stock) {
+        super(need_more_stock);
+    }
+
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+}
---
+++ src/main/java/com/brandon/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.brandon.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String u) {
+    }
+}
