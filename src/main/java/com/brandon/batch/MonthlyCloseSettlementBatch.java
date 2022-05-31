9d5c6b39e56d2fdb9056e7363e68923b84f39188
brandon <dmdjqn@gmail.com>
2022-05-31 21:19:10 +0900

    feat: 월정산 배치 기능추가

--- src/main/java/com/brandon/batch/MonthlyCloseSettlementBatch.java
+++ src/main/java/com/brandon/batch/MonthlyCloseSettlementBatch.java
@@ -5,26 +5,28 @@ import com.brandon.repository.SettlementCustomRepository;
 import com.brandon.repository.SettlementRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Service
+@Component
 @RequiredArgsConstructor
-public class MonthlyCloseSettlementBatchService {
+public class MonthlyCloseSettlementBatch {
 
     private final SettlementRepository settlementRepository;
     private final SettlementCustomRepository settlementCustomRepository;
 
+    private final LocalDateTime START_DATE_TIME = LocalDate.now().minusDays(1).withDayOfMonth(1).atTime(0, 0, 0, 0);
+    private final LocalDateTime END_DATE_TIME = LocalDate.now().minusDays(1).atTime(23,59,59,1000000000-1);
+
     //매달 1일 03시에 실행
     //@Scheduled(cron = "0 0 0 3 * *")
+    @Transactional
     public void updateFirstProductPage() {
-
-        LocalDateTime start = LocalDate.now().minusDays(1).withDayOfMonth(1).atTime(0,0,0,0);
-        LocalDateTime end = LocalDate.now().minusDays(1).atTime(23,59,59,1000000000-1);
-
-        settlementCustomRepository.settlement(start,end)
+        settlementCustomRepository.settlement(START_DATE_TIME,END_DATE_TIME)
                 .stream()
                 .map(dto -> new Settlement(dto.getMemberId(),
                         dto.getMemberName(),
@@ -38,5 +40,7 @@ public class MonthlyCloseSettlementBatchService {
                 .forEach(settlement -> settlementRepository.save(settlement));
 
     }
+
+
 
 }
