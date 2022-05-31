72752b9fea2a5f5165bcb44bc8ca35b37314324a
brandon <dmdjqn@gmail.com>
2022-05-31 21:27:40 +0900

    refactor: 월정산엔티티 리팩토링

--- src/main/java/com/brandon/domain/Settlement.java
+++ src/main/java/com/brandon/domain/Settlement.java
@@ -2,7 +2,9 @@ package com.brandon.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,10 +12,11 @@ import javax.persistence.Id;
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Settlement {
-
     @Id
     @GeneratedValue
+    @Column(name = "settlement_id")
     private Long id;
 
     private Long memberId;
