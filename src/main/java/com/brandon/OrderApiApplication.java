e215525a50c9e534166b8316773b4eade6d35ba8
brandon <dmdjqn@gmail.com>
2022-05-31 21:28:02 +0900

    feat: 캐싱기능추가

--- src/main/java/com/brandon/OrderApiApplication.java
+++ src/main/java/com/brandon/OrderApiApplication.java
@@ -2,14 +2,16 @@ package com.brandon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class OrderApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OrderApiApplication.class, args);
     }
-
 }
